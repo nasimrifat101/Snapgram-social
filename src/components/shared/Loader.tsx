fcb17ff322fa69b798af001f935048c3ccd251d0
nasimrifat101 <nasimrifat101@gmail.com>
2023-12-09 16:41:38 +0600

    sign up page complete

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,10 @@
+
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+      <img src="/public/assets/icons/loader.svg" alt="Loading.." />
+    </div>
+  );
+};
+
+export default Loader;
